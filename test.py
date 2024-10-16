09a2909aae8608faae5e8d31d3a47e217a9f5acc
nishikatakagi <quocviet242003@gmail.com>
2024-10-16 17:29:20 +0700

    feat: function check quantity in the store

--- test.py
+++ test.py
@@ -35,6 +35,29 @@ def count_records_with_sl_phim(elastic_client, index_name):
     
     # Trả về số lượng bản ghi có trường sl_phim tồn tại
     return response['count']
+
+def count_records_with_sl_phim_2(elastic_client, index_name):
+    # Truy vấn Elasticsearch để đếm số lượng bản ghi có trường sl_phim tồn tại
+    query = {
+        "query": {
+            "exists": {
+                "field": "sl_phim"  # Kiểm tra xem trường sl_phim có tồn tại hay không
+            }
+        }
+    }
+
+    # Thực hiện truy vấn và lấy tổng số lượng kết quả
+    response = elastic_client.count(
+        index=index_name,
+        body=query
+    )
+    
+    # Trả về số lượng bản ghi có trường sl_phim tồn tại
+    return response['count']
+
+# Ví dụ gọi hàm
+count = count_records_with_sl_phim_2(elastic_local, 'khoso')
+print(f"Số lượng bản ghi có trường 'sl_phim' tồn tại: {count}") 
   
 count = count_records_with_sl_phim(elastic_local, 'khoso')
 print(f"Số lượng bản ghi không có trường 'sl_phim' tồn tại: {count}")
